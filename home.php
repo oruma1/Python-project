1e6b7b361233498146dd0fe7fe1fd9d1b0ca505f
BrianKawinzi <brian.mutisya@strathmore.edu>
2024-06-19 05:15:15 +0300

    modified:   home.php

--- home.php
+++ home.php
@@ -5,12 +5,54 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>home</title>
-
+<!-- swiper css link -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+<!-- font awesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudfare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
+<!-- custom css file links -->
         <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
+
+<!--header section -->
+<section class="header">
+
+<a href= home.php class = logo> AbANTU TRAVELS </a>
+
+<nav class="navbar">
+
+<a href= home.php>home</a>
+<a href= home.php>home</a>
+<a href= home.php>home</a>
+<a href= home.php>home</a>
+</nav>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<-- custom js file link -->
+<script src="js/script.js"></script>
+
+
+<!-- swiper link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+</html>
 </body>
 </html>
